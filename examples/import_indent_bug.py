5fc49c844bae5101545b964f55f10ee26a5da531
nuwandavek <vivekaithal44@gmail.com>
2023-02-25 17:52:12 -0800

    add edit

--- examples/import_indent_bug.py
+++ examples/import_indent_bug.py
@@ -1,10 +1,17 @@
-import Random
-a = random.randint(1,12)
-  b = random.randint(1,12)
-for i in range(10):
-    question = "What is "+a+" x "+b+"? "
-    answer = input(question)
-    if answer =a*b
-        print (Well done!)
-    else:
-        print("No.")
+import random
+
+
+def main():
+    a = random.randint(1, 12)
+    b = random.randint(1, 12)
+    for i in range(10):
+        question = "What is " + str(a) + " x " + str(b) + "? "
+        answer = input(question)
+        if answer == str(a * b):
+            print("Well done!")
+        else:
+            print("No.")
+
+
+if __name__ == "__main__":
+    main()
